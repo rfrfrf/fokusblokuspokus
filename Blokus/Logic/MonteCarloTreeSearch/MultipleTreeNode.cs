4c0526fc983075c40f0498aa5f48dde93f0d9013
tommyaf1 <tommyaf1@gmail.com>
2010-05-05 11:39:42 +0000

    Ruszyłem MCTS

---
+++ Blokus/Logic/MonteCarloTreeSearch/MultipleTreeNode.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Blokus.Logic.MonteCarloTreeSearch
+{
+    public class MultipleTreeNode
+    {
+        public int visitCount = 0;
+        public int victoryCount = 0;
+        public Move move;
+
+        public List<MultipleTreeNode> childrenList = null;
+        public MultipleTreeNode parentNode = null;
+
+        public MultipleTreeNode()
+        {
+            visitCount = 0;
+            victoryCount = 0;
+            move = null;
+            parentNode = null;
+            childrenList = null;
+        }
+
+        public MultipleTreeNode(Move _move, MultipleTreeNode _parentNode)
+        {
+            visitCount = 0;
+            victoryCount = 0;
+            move = _move;
+            parentNode = _parentNode;
+            childrenList = null;
+        }
+
+        public void ComputeAverage(int r)
+        {
+            //TODO: zrobic przeliczanie wartosci wezla
+            return;
+        }
+    }
+}
