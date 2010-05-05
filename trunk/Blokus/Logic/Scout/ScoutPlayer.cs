68a059d83ba9ef3da8e3e705ef3511b520d12750
maihoa.pw <maihoa.pw@7e2c5dc1-4ba5-51cb-cb10-541968f474f6>
2010-05-05 14:10:45 +0000

    Nigga scout działa

--- trunk/Blokus/Logic/Scout/ScoutPlayer.cs
+++ trunk/Blokus/Logic/Scout/ScoutPlayer.cs
@@ -8,19 +8,21 @@ namespace Blokus.Logic.Scout
 {
     class ScoutPlayer : AIPlayer
     {
+        private const int MaxDepth = 8;
         private const int MaxTreeRank = 7;
-        private ScoutHeuristics _Heursitics = new ScoutHeuristics();
+        private AlphaBetaHeuristics _Heursitics = new AlphaBetaHeuristics();
         private Move _LastMove;
 
         public override Move GetMove(GameState gameState)
         {
             _LastMove = null;
-            Scout(gameState);
+            NegaScout(gameState, double.NegativeInfinity, double.PositiveInfinity, 0);
             return _LastMove;
         }
 
         // condition <= ozn r
         // condition < ozn m
+        /*
         private bool TestNode(GameState gameState, double value, char condition)
         {
             _Heursitics.SortHand(gameState);
@@ -75,10 +77,138 @@ namespace Blokus.Logic.Scout
             }
             return value;
         }
+        */
+
+        private double AlphaBeta(GameState gameState, double alpha, double beta, int depth )
+        {
+            if (depth == 0) //osiagnieto maksymalny poziom rekurencji
+            {
+                return _Heursitics.GetBoardEvaluation(gameState);
+            }
+            _Heursitics.SortHand(gameState); //posortuj klocki gracza by najlepsze byly na poczatku
+            var moves = GameRules.GetMoves(gameState, MaxTreeRank); //pobierz MaxTreeRank pierwszych dostepnych ruchow
+
+            if (moves.Count == 0)
+            {
+                return GameRules.GetGameResult(gameState); // pozycja koncowa
+            }
+
+            _Heursitics.SortMoves(gameState, moves); //posortuj ruchy by najlepsze byly na poczatku
+
+            foreach (var move in moves)
+            {               
+                //wczuj sie w przeciwnika
+                gameState.AddMove(move); //poloz na planszy klocek
+                gameState.SwapCurrentPlayer(); //zmien aktywnego gracza na przeciwnego
+                var lastMoveCopy = _LastMove; //zapamietaj najlepszy ruch bo rekurencyjne wywolanie go nadpisze
+                //znajdz ruch przeciwnka
+                double newalpha = -AlphaBeta(gameState, -beta, -alpha, depth - 1);
+                //posprzataj
+                _LastMove = lastMoveCopy; //przywroc najlepszy ruch
+                gameState.SwapCurrentPlayer(); //przywroc aktywnego gracza                 
+                gameState.DelMove(move); //zdejmij klocek z planszy
+
+                if (newalpha > alpha)
+                {
+                    alpha = newalpha;
+                    _LastMove = move;
+                }
+                if (beta < alpha)
+                {
+                    break;
+                }
+            }
+            return alpha;
+        }
+
+
+        /*
+         * function negascout(node, depth, α, β)
+    if node is a terminal node or depth = 0
+        return the heuristic value of node
+    b := β                                          (* initial window is (-β, -α) *)
+    foreach child of node
+        a := -negascout (child, depth-1, -b, -α)
+        if a>α
+            α := a
+        if α≥β
+            return α                                (* Beta cut-off *)
+        if α≥b                                      (* check if null-window failed high*)
+           α := -negascout(child, depth-1, -β, -α)  (* full re-search *)
+           if α≥β
+               return α                             (* Beta cut-off *)    
+        b := α+1                                    (* set new null window *)             
+    return α
+
+         * */
+        private double NegaScout (GameState gameState, double alpha, double beta, int d )
+        {                     /* compute minimax value of position p */
+           double a, b, t;
+
+           if ( d == MaxDepth )
+              return _Heursitics.GetBoardEvaluation(gameState);
+
+           _Heursitics.SortHand(gameState); //posortuj klocki gracza by najlepsze byly na poczatku
+           var moves = GameRules.GetMoves(gameState, MaxTreeRank); //pobierz MaxTreeRank pierwszych dostepnych ruchow
+
+           if (moves.Count == 0)
+           {
+               return GameRules.GetGameResult(gameState); // pozycja koncowa
+           }
+
+           _Heursitics.SortMoves(gameState, moves); //posortuj ruchy by najlepsze byly na poczatku
+
+           a = alpha;
+           b = beta;
+           bool first = true;
+           double maxVal = double.NegativeInfinity;
+           Move bestMove = null;
+           foreach(var move in moves)
+           {
+               //wczuj sie w przeciwnika
+              gameState.AddMove(move); //poloz na planszy klocek
+              gameState.SwapCurrentPlayer(); //zmien aktywnego gracza na przeciwnego
+               //znajdz ruch przeciwnka
+              t = -NegaScout(gameState, -b, -a, d + 1);
+
+              if (t > maxVal)
+              {
+                  maxVal = t;
+                  bestMove = move;
+              }
+              
+
+              if ((t > a) && (t < beta) && !first && (d < MaxDepth - 1))
+              {
+                  a = -NegaScout(gameState, -beta, -t, d + 1);     /* re-search */
+                  if (a > maxVal)
+                  {
+                      maxVal = a;
+                      bestMove = move;
+                  }
+              }
+              first = false;
+              //przywroc najlepszy ruch
+              gameState.SwapCurrentPlayer(); //przywroc aktywnego gracza                 
+              gameState.DelMove(move); //zdejmij klocek z planszy
+
+              
+
+              a = Math.Max( a, t );
+              if (a >= beta)
+              {
+                  _LastMove = bestMove;
+                  return a;
+              }
+              b = a + 1;                      /* set new null window */
+           }
+           _LastMove = bestMove;
+           return a;
+        }
 
         public override string ToString()
         {
-            return "Scout";
+            return "Nigga - Scout";
         }
     }
 }
