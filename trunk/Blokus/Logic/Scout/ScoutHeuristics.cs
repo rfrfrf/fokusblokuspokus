3b8b5f7c2d4349aa5e96ebe1dd81b0f1b6b1f131
maihoa.pw <maihoa.pw@7e2c5dc1-4ba5-51cb-cb10-541968f474f6>
2010-05-05 12:33:57 +0000

    Scout

---
+++ trunk/Blokus/Logic/Scout/ScoutHeuristics.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Blokus.Logic.Scout
+{
+    class ScoutHeuristics
+    {
+        /// <summary>
+        /// dla pomaranczowego gracza promuje ruchy w kierunku lewego gornego rogu planszy
+        /// dla fioletowego gracza promuje ruchy w kierunku prawego dolnego rogu planszy
+        /// </summary>
+        public double GetBoardEvaluation(GameState gameState)
+        {
+            var elements = gameState.Board.BoardElements;
+            double result = 0.0;
+
+            if (gameState.CurrentPlayerColor == Player.Orange)
+            {
+                for (int i = 0; i < elements.GetLength(0); i++)
+                {
+                    for (int j = 0; j < elements.GetLength(1); j++)
+                    {
+                        if (elements[i, j] == Player.Orange)
+                        {
+                            result += 1.0 / (i + j+ 5);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < elements.GetLength(0); i++)
+                {
+                    for (int j = 0; j < elements.GetLength(1); j++)
+                    {
+                        if (elements[i, j] == Player.Violet)
+                        {
+                            result += 1.0 / (elements.GetLength(0) - i + elements.GetLength(1)-j+5);
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// na poczatku listy klockow ustawia klocki o najwiekszej liczbie rogow lub najwiekszej liczbie elementow
+        /// </summary>
+        /// <param name="gameState"></param>
+        public void SortHand(GameState gameState)
+        {           
+            gameState.CurrentPlayerHand.HandPieces.Sort((x, y) =>
+            {
+                int a = x.Variants[0].CornersIndicies.Length;
+                int b = y.Variants[0].CornersIndicies.Length;
+                if (a != b)
+                {
+                    return b.CompareTo(a);
+                }
+                return y.Variants[0].Squares.Length.CompareTo(x.Variants[0].Squares.Length);
+            });
+        }
+
+        /// <summary>
+        /// dla pomaranczowego gracza promuje ruchy w kierunku lewego gornego rogu planszy
+        /// dla fioletowego gracza promuje ruchy w kierunku prawego dolnego rogu planszy
+        /// </summary>
+        /// <param name="gameState"></param>
+        /// <param name="moves"></param>
+        public void SortMoves(GameState gameState, List<Move> moves)
+        {
+            int multiplier = gameState.CurrentPlayerColor == Player.Violet ? 1 : -1;
+            moves.Sort((x, y) =>
+            {
+                int a = x.Position.X + x.Position.Y;
+                int b = y.Position.X + y.Position.Y;
+                if (a != b)
+                {
+                    return b.CompareTo(a) * multiplier;
+                }
+                return y.PieceVariant.Squares.Length.CompareTo(x.PieceVariant.Squares.Length) * multiplier;
+            });
+        }
+    }
+}
--- trunk/Blokus/Logic/Scout/ScoutPlayer.cs
+++ trunk/Blokus/Logic/Scout/ScoutPlayer.cs
@@ -2,15 +2,79 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Blokus.Logic.AlphaBeta;
 
 namespace Blokus.Logic.Scout
 {
     class ScoutPlayer : AIPlayer
     {
+        private const int MaxTreeRank = 7;
+        private ScoutHeuristics _Heursitics = new ScoutHeuristics();
+        private Move _LastMove;
+
         public override Move GetMove(GameState gameState)
         {
-            return null; //TODO: wyszukiwanie ruchu scoutem
-        }        
+            _LastMove = null;
+            Scout(gameState);
+            return _LastMove;
+        }
+
+        // condition <= ozn r
+        // condition < ozn m
+        private bool TestNode(GameState gameState, double value, char condition)
+        {
+            _Heursitics.SortHand(gameState);
+            var moves = GameRules.GetMoves(gameState, MaxTreeRank);
+
+            if (moves.Count == 0)
+                switch (condition)
+                {
+                    case 'm':
+                        return (_Heursitics.GetBoardEvaluation(gameState) > value);
+                    case 'r':
+                        return (_Heursitics.GetBoardEvaluation(gameState) >= value);
+                }
+
+            _Heursitics.SortMoves(gameState, moves);
+            if (gameState.CurrentPlayerColor == Player.Orange)
+                foreach (var move in moves)
+                {
+                    gameState.AddMove(move);
+                    if (TestNode(gameState, value, condition) == true)
+                        return true;
+                }
+
+            if (gameState.CurrentPlayerColor == Player.Violet)
+                foreach (var move in moves)
+                {
+                    gameState.AddMove(move);
+                    if (TestNode(gameState, value, condition) == false)
+                        return false;
+                }
+            return false;
+        }
+
+        private double Scout(GameState gameState)
+        {
+            double value;
+            var moves = GameRules.GetMoves(gameState, MaxTreeRank);
+            
+            if (moves.Count == 0)
+                return _Heursitics.GetBoardEvaluation(gameState);
+            
+            gameState.AddMove(moves[0]);
+            value = Scout(gameState);
+
+            foreach (var move in moves)
+            {
+                gameState.AddMove(move);
+                value = Scout(gameState);
+
+                if ((gameState.CurrentPlayerColor == Player.Orange && TestNode(gameState, value, 'm') == true) || (gameState.CurrentPlayerColor == Player.Violet && TestNode(gameState, value, 'r') == false))
+                    value = Scout(gameState);
+            }
+            return value;
+        }
 
         public override string ToString()
         {
