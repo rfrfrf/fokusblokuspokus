09626d1414c5db87696e1a5dbeac68e9c194fed6
masterkielbaster <masterkielbaster@7e2c5dc1-4ba5-51cb-cb10-541968f474f6>
2010-05-17 22:28:16 +0000

    chyba działą gra pomarańczowy mcts vs fioletowy nie-mcts

--- TestProject1/SerializationTest.cs
+++ TestProject1/SerializationTest.cs
@@ -6,6 +6,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
 using Blokus.Logic;
+using Blokus.Logic.MCTS;
 
 namespace TestProject1
 {
@@ -61,6 +62,35 @@ namespace TestProject1
         // public void MyTestCleanup() { }
         //
         #endregion
+
+        [TestMethod]
+        public void NodeTest()
+        {
+            var root = new Node();
+            var move = new Move(3);
+            root.Children.Add(move.SerializedMove, new Node() { VisitCount = 666, WinCount = 12 });
+            move = new Move(1);
+            root.Children.Add(move.SerializedMove, new Node());
+            move = new Move(2);
+            root[3].Children.Add(move.SerializedMove, new Node() { VisitCount = 9, WinCount = 10 });
+
+            var ms = new MemoryStream();
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(ms, root);
+            ms.Position = 0;
+            var actual = (Node)bf.Deserialize(ms);
+
+
+            Assert.AreEqual(12, actual[3].WinCount);
+            Assert.AreEqual(666, actual[3].VisitCount);
+            Assert.IsTrue(actual.Children.Count == 2);
+            Assert.IsTrue(actual.Children.ContainsKey(3));
+            Assert.IsTrue(actual.Children.ContainsKey(1));
+            Assert.IsTrue(actual[3].Children.Count == 1);
+            Assert.IsTrue(actual[3].Children.ContainsKey(2));
+            Assert.AreEqual(10, actual[3][2].WinCount);
+            Assert.AreEqual(9, actual[3][2].VisitCount);            
+        }
 
         [TestMethod]
         public void MoveTest()
